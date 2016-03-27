790ee2b09a7235d2700baa3b7520c50352803a4f
TsomeTime <644168816@qq.com>
2016-03-27 14:07:11 +0800

    init

---
+++ test2016/test2016/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test2016
+//
+//  Created by tt on 16/3/27.
+//  Copyright © 2016年 leo.liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
